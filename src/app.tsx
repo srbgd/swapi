e3bd1cf25068d7d2d4fa39d96be50c6057fd823c
Sergey Bogdanik <Sergey.Bogdanik@acronis.com>
2021-02-22 01:41:18 +0300

    Create a basic page to show films info

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 class Goto extends React.Component<any, any> {
     render() {
       return (
-        <button onClick={() => this.props.setLocation(this.props.l)}> 
+        <button onClick={() => this.props.App.setLocation(this.props.l)}> 
             {this.props.n}
         </button>
       )
@@ -13,19 +13,77 @@ class Goto extends React.Component<any, any> {
 
 class Main extends React.Component<any, any> {
     render() {
-      return (
-        <div>
-            <p>Resources:</p>
-            <Goto l='/films' n='Films' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/people' n='People' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/planets' n='Planets' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/species' n='Species' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/starships' n='Starships' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/vehicles' n='Vehicles' setLocation={this.props.setLocation} /><br/>
-            <Goto l='/' n='Main' setLocation={this.props.setLocation} />
-        </div>
-      )
-      
+        return (
+            <div>
+                <p>Resources:</p>
+                <Goto l='/films/1' n='Films' App={this.props.App} /><br/>
+                <Goto l='/people/1' n='People' App={this.props.App} /><br/>
+                <Goto l='/planets/1' n='Planets' App={this.props.App} /><br/>
+                <Goto l='/species/1' n='Species' App={this.props.App} /><br/>
+                <Goto l='/starships/1' n='Starships' App={this.props.App} /><br/>
+                <Goto l='/vehicles/1' n='Vehicles' App={this.props.App} /><br/>
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component<any, any> {
+
+    next(l){
+        let n = l.indexOf('/', 1)
+        return l.substr(0, n) + '/' + (parseInt(l.substr(n + 1)) + 1)
+    }
+
+    back(l){
+        let n = l.indexOf('/', 1)
+        return l.substr(0, n) + '/' + (parseInt(l.substr(n + 1)) - 1)
+    }
+
+    render() {
+        let location = this.props.App.state.location
+        return (
+            <div>
+                <p>Page Location: {location}</p>
+                {
+                    location != '/' &&
+                    <div>
+                        <Goto l={this.next(location)} n='Next' App={this.props.App} /><br/>
+                        <Goto l={this.back(location)} n='Back' App={this.props.App} /><br/>
+                        <Goto l='/' n='Main' App={this.props.App} /><br/>
+                    </div>
+                }
+            </div>
+      )  
+    }
+}
+
+class Data extends React.Component<any, any> {
+    render() {
+        console.log(this.props.data)
+        let listData = this.props.data.map( (e) => {return <p>{e.k}: {e.v}</p>} )
+        console.log(listData)
+        return (
+            <div>
+                {listData}
+            </div>
+      )  
+    }
+}
+
+class Films extends React.Component<any, any> {
+    render() {
+        const data = [
+            {k: 'AAA', v: '123'},
+            {k: 'BBB', v: '456'},
+            {k: 'CCC', v: '789'}
+        ]
+        console.log(this.props.App)
+        return (
+            <div>
+                <Header App={this.props.App} />
+                <Data data={data} App={this.props.App} />
+            </div>
+      )  
     }
 }
 
@@ -41,13 +99,20 @@ class App extends React.Component<any, any> {
     }
 
     render() {
-      return (
-        <div>
-            <p>Location: {this.state.location}</p>
-            <Main setLocation={this.setLocation}/>
-        </div>
-      )
+        let page = null
+        let s = this.state.location.substr(0, this.state.location.indexOf('/', 1))
+        if(s == '/films'){
+            page = <Films App={this} />
+        }else{
+            page = <Main App={this} />
+        }
+        return (
+            <div>
+                <p>App Location: {this.state.location}</p>
+                {page}
+            </div>
+        )
     }
-  }
+}
   
   export default App;
