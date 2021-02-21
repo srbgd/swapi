0f671b45f9ff87016cb8cd51091f29e5cda68377
Sergey Bogdanik <Sergey.Bogdanik@acronis.com>
2021-02-22 00:19:38 +0300

    Add main page

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-let location = '/';
-
 class Goto extends React.Component<any, any> {
-
-    handleClick() {
-        location = this.props.n;
-    }
-
     render() {
       return (
-        <button onClick={this.handleClick}> 
+        <button onClick={() => this.props.setLocation(this.props.l)}> 
             {this.props.n}
         </button>
       )
@@ -18,12 +11,40 @@ class Goto extends React.Component<any, any> {
     }
 }
 
-class App extends React.Component<any, any> {
+class Main extends React.Component<any, any> {
     render() {
       return (
         <div>
-            <p>Location: {location}</p>
-            <Goto l='/films' n='Films'/>
+            <p>Resources:</p>
+            <Goto l='/films' n='Films' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/people' n='People' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/planets' n='Planets' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/species' n='Species' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/starships' n='Starships' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/vehicles' n='Vehicles' setLocation={this.props.setLocation} /><br/>
+            <Goto l='/' n='Main' setLocation={this.props.setLocation} />
+        </div>
+      )
+      
+    }
+}
+
+class App extends React.Component<any, any> {
+    constructor(props, state) {
+        super(props);
+        this.setLocation = this.setLocation.bind(this);
+        this.state = {location: '/'};
+    }
+
+    setLocation(l){
+        this.setState({location: l});
+    }
+
+    render() {
+      return (
+        <div>
+            <p>Location: {this.state.location}</p>
+            <Main setLocation={this.setLocation}/>
         </div>
       )
     }
